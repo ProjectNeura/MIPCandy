f26c79885ab9679a754e348aeae2961c71db5790
ATATC <futerry@outlook.com>
2025-09-10 17:00:13 -0400

    Refactored datasets to introduce generic types, improve inheritance, and streamline constructors.

--- mipcandy/data/dataset.py
+++ mipcandy/data/dataset.py
@@ -3,7 +3,7 @@ from os import PathLike, listdir, makedirs
 from os.path import exists
 from random import choice, randint
 from shutil import move, copy
-from typing import Literal, override, Self, Sized, Sequence
+from typing import Literal, override, Self, Sequence, TypeVar, Generic, Any
 
 import torch
 from torch.utils.data import Dataset
@@ -11,6 +11,25 @@ from torch.utils.data import Dataset
 from mipcandy.data.io import load_image
 from mipcandy.layer import HasDevice
 from mipcandy.types import Transform
+
+T = TypeVar("T")
+
+
+class KFPicker(object, metaclass=ABCMeta):
+    @staticmethod
+    @abstractmethod
+    def pick(self, choices: Sequence[T], fold: Literal[0, 1, 2, 3, 4, "all"]) -> Sequence[T]:
+        raise NotImplementedError
+
+
+class OrderedKFPicker(KFPicker):
+    @staticmethod
+    @override
+    def pick(self, choices: Sequence[T], fold: Literal[0, 1, 2, 3, 4, "all"]) -> Sequence[T]:
+        if fold == "all":
+            return choices[::4]
+        size = len(choices) // 5
+        return choices[size * fold: size * (fold + 1)]
 
 
 class Loader(object):
@@ -20,82 +39,76 @@ class Loader(object):
         return load_image(path, is_label=is_label, device=device, **kwargs)
 
 
-class _AbstractDataset(Dataset, Loader, HasDevice, Sized, metaclass=ABCMeta):
-    pass
-
-
-class UnsupervisedDataset(_AbstractDataset, metaclass=ABCMeta):
+class _AbstractDataset(Dataset, Loader, HasDevice, Generic[T], Sequence[T], metaclass=ABCMeta):
     @abstractmethod
-    def load(self, idx: int) -> torch.Tensor:
+    def load(self, idx: int) -> T:
         raise NotImplementedError
 
     @override
-    def __getitem__(self, idx: int) -> torch.Tensor:
+    def __getitem__(self, idx: int) -> T:
         return self.load(idx)
 
 
-class SupervisedDataset(_AbstractDataset, metaclass=ABCMeta):
-    @abstractmethod
-    def load(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
-        raise NotImplementedError
-
-    @override
-    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
-        return self.load(idx)
+D = TypeVar("D", bound=Sequence)
 
 
-class DatasetFromMemory(UnsupervisedDataset):
-    def __init__(self, images: Sequence[torch.Tensor],
-                 device: torch.device | str = "cpu") -> None:
+class UnsupervisedDataset(_AbstractDataset[torch.Tensor], Generic[D], metaclass=ABCMeta):
+    def __init__(self, images: D, *, device: torch.device | str = "cpu") -> None:
         super().__init__(device)
-        self._images: Sequence[torch.Tensor] = images
+        self._images: D = images
+
+    @override
+    def __len__(self) -> int:
+        return len(self._images)
+
+
+class SupervisedDataset(_AbstractDataset[tuple[torch.Tensor, torch.Tensor]], Generic[D], metaclass=ABCMeta):
+    def __init__(self, images: D, labels: D, *, device: torch.device | str = "cpu") -> None:
+        super().__init__(device)
+        if len(images) != len(labels):
+            raise ValueError(f"Unmatched number of images {len(images)} and labels {len(labels)}")
+        self._images: D = images
+        self._labels: D = labels
+
+    @override
+    def __len__(self) -> int:
+        return len(self._images)
+
+
+class DatasetFromMemory(UnsupervisedDataset[Sequence[torch.Tensor]]):
+    def __init__(self, images: Sequence[torch.Tensor], device: torch.device | str = "cpu") -> None:
+        super().__init__(images, device=device)
 
     @override
     def load(self, idx: int) -> torch.Tensor:
         return self._images[idx].to(self._device)
 
-    @override
-    def __len__(self) -> int:
-        return len(self._images)
 
-
-class MergedDataset(SupervisedDataset):
+class MergedDataset(SupervisedDataset[UnsupervisedDataset]):
     def __init__(self, images: UnsupervisedDataset, labels: UnsupervisedDataset, *,
                  device: torch.device | str = "cpu") -> None:
-        super().__init__(device)
-        if len(images) != len(labels):
-            raise ValueError("Unmatched number of images and labels")
-        self._images: UnsupervisedDataset = images
-        self._labels: UnsupervisedDataset = labels
+        super().__init__(images, labels, device=device)
 
     @override
     def load(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
         return self._images[idx].to(self._device), self._labels[idx].to(self._device)
 
-    @override
-    def __len__(self) -> int:
-        return len(self._images)
 
-
-class NNUNetDataset(SupervisedDataset):
+class NNUNetDataset(SupervisedDataset[list[str]]):
     def __init__(self, folder: str | PathLike[str], *, split: Literal["Tr", "Ts"] = "Tr", prefix: str = "",
                  align_spacing: bool = False, image_transform: Transform | None = None,
                  label_transform: Transform | None = None, device: torch.device | str = "cpu") -> None:
-        super().__init__(device)
+        images: list[str] = [f for f in listdir(f"{folder}/images{split}") if f.startswith(prefix)]
+        images.sort()
+        labels: list[str] = [f for f in listdir(f"{folder}/labels{split}") if f.startswith(prefix)]
+        labels.sort()
+        super().__init__(images, labels, device=device)
         self._folder: str = folder
         self._split: str = split
         self._prefix: str = prefix
         self._align_spacing: bool = align_spacing
         self._image_transform: Transform | None = image_transform
         self._label_transform: Transform | None = label_transform
-        self._images: list[str] = [f for f in listdir(f"{folder}/images{split}") if f.startswith(prefix)]
-        self._images.sort()
-        self._labels: list[str] = [f for f in listdir(f"{folder}/labels{split}") if f.startswith(prefix)]
-        self._labels.sort()
-        self._num_cases: int = len(self._images)
-        num_labels = len(self._labels)
-        if num_labels != self._num_cases:
-            raise FileNotFoundError(f"Inconsistent number of labels ({num_labels}) and images ({self._num_cases})")
 
     @staticmethod
     def _create_subset(folder: str) -> None:
@@ -106,7 +119,7 @@ class NNUNetDataset(SupervisedDataset):
     def divide(self) -> tuple[list[int], list[int]]:
         positives = []
         negatives = []
-        for i in range(self._num_cases):
+        for i in range(len(self)):
             _, label = self[i]
             (positives if label.max() > 0 else negatives).append(i)
         return positives, negatives
@@ -115,15 +128,15 @@ class NNUNetDataset(SupervisedDataset):
               positive_only: bool = False) -> Self:
         if split == self._split:
             raise FileExistsError(f"Split {split} already exists")
-        if not (0 < size < self._num_cases):
-            raise ValueError(f"Invalid test set size {size}, expected (0, {self._num_cases})")
+        if not (0 < size < len(self)):
+            raise ValueError(f"Invalid test set size {size}, expected (0, {len(self)})")
         images_folder = f"{self._folder}/images{split}"
         labels_folder = f"{self._folder}/labels{split}"
         self._create_subset(images_folder)
         self._create_subset(labels_folder)
         op = move if exclusive else copy
         images, labels = self._images.copy(), self._labels.copy()
-        num_cases = self._num_cases
+        num_cases = len(self)
         positives = self.divide()[0]
         if positive_only and len(positives) < size:
             raise RuntimeError(f"Not enough positive cases {len(positives)}/{size}")
@@ -142,13 +155,8 @@ class NNUNetDataset(SupervisedDataset):
             op(f"{self._folder}/labels{self._split}/{label}", f"{labels_folder}/{label}")
         if exclusive:
             self._images, self._labels = images, labels
-            self._num_cases -= size
         return NNUNetDataset(self._folder, split=split, prefix=self._prefix, image_transform=self._image_transform,
                              label_transform=self._label_transform)
-
-    @override
-    def __len__(self) -> int:
-        return self._num_cases
 
     @override
     def load(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
