3dc198b9dc1861d2d073d962fc5ae7746e52163e
ATATC <futerry@outlook.com>
2025-10-08 11:44:14 -0400

    Enhanced `download_dataset()` to support custom console for improved logging flexibility. (#49)

--- mipcandy/data/download.py
+++ mipcandy/data/download.py
@@ -2,18 +2,20 @@ from os import PathLike
 from zipfile import ZipFile
 
 from requests import get
+from rich.console import Console
 from rich.progress import track
 
 
-def download_dataset(name: str, to: str | PathLike[str], *, endpoint: str = "cds.projectneura.org") -> None:
+def download_dataset(name: str, to: str | PathLike[str], *, endpoint: str = "cds.projectneura.org",
+                     console: Console = Console()) -> None:
     to_zip = f"{to}.zip"
     with get(f"https://{endpoint}/{name}.zip", stream=True) as response:
         response.raise_for_status()
         with open(to_zip, "wb") as f:
-            for chunk in track(response.iter_content(chunk_size=8192), description="Downloading..."):
+            for chunk in track(response.iter_content(chunk_size=8192), description="Downloading...", console=console):
                 f.write(chunk)
-    print(f"Dataset successfully downloaded as {to_zip}")
-    print("Unzipping...")
+    console.print(f"Dataset successfully downloaded as {to_zip}")
+    console.print("Unzipping...")
     with ZipFile(to_zip, "r") as zip_ref:
         zip_ref.extractall(to)
-    print(f"Dataset successfully extracted to {to}")
+    console.print(f"Dataset successfully extracted to {to}")
