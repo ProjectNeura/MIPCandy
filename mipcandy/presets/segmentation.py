c4b6b3207b792701d60bd94f506ca19646a7589f
Steven Chen <steven2381824724@gmail.com>
2025-12-08 11:49:12 -0500

    SlidingTrainer: compute validation metrics on reconstructed full volume

--- mipcandy/presets/segmentation.py
+++ mipcandy/presets/segmentation.py
@@ -73,12 +73,11 @@ class SlidingSegmentationTrainer(SlidingTrainer, SegmentationTrainer, metaclass=
         return SegmentationTrainer.backward(self, images, labels, toolbox)
 
     @override
-    def validate_case_windowed(self, images: torch.Tensor, labels: torch.Tensor, toolbox: TrainerToolbox,
-                               metadata: SWMetadata) -> tuple[float, dict[str, float], torch.Tensor]:
-        masks = (toolbox.ema if toolbox.ema else toolbox.model)(images)
-        loss, metrics = toolbox.criterion(masks, labels)
-        return -loss.item(), metrics, masks
+    def compute_metrics(self, output: torch.Tensor, label: torch.Tensor, toolbox: TrainerToolbox) -> tuple[
+        float, dict[str, float]]:
+        loss, metrics = toolbox.criterion(output, label)
+        return -loss.item(), metrics
 
     @override
-    def get_window_shape(self):
+    def get_window_shape(self) -> tuple[int, int] | tuple[int, int, int]:
         return self.sliding_window_shape
