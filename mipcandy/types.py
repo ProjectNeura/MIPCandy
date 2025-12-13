7dd3bd2a0f0667b8f1daeacf2aee911e7f729dad
ATATC <futerry@outlook.com>
2025-12-12 22:07:26 -0500

    Added `Shape2d`, `Shape3d`, and `Shape` type definitions. (#137)

--- mipcandy/types.py
+++ mipcandy/types.py
@@ -12,3 +12,6 @@ type Transform = nn.Module | Compose
 type SupportedPredictant = Sequence[torch.Tensor] | str | PathLike[str] | Sequence[str] | torch.Tensor
 type Colormap = Sequence[int | tuple[int, int, int]]
 type Device = torch.device | str
+type Shape2d = tuple[int, int]
+type Shape3d = tuple[int, int, int]
+type Shape = Shape2d | Shape3d
