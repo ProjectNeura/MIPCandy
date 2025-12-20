853b61d980450163b8136188de93ef3fe0a5c8a0
ATATC <futerry@outlook.com>
2025-12-19 23:44:17 -0500

    Refactored `UNetTrainer` to use `SlidingSegmentationTrainer` with sliding window support. (#133)

--- unet.py
+++ unet.py
@@ -3,10 +3,13 @@ from typing import override
 from monai.networks.nets import BasicUNet
 from torch import nn
 
-from mipcandy import SegmentationTrainer, AmbiguousShape
+from mipcandy import SlidingSegmentationTrainer, AmbiguousShape
 
 
-class UNetTrainer(SegmentationTrainer):
+class UNetTrainer(SlidingSegmentationTrainer):
+    sliding_window_shape = (64, 64, 64)
+    sliding_window_batch_size = 1
+
     @override
     def build_network(self, example_shape: AmbiguousShape) -> nn.Module:
         return BasicUNet(3, example_shape[0], 4)
