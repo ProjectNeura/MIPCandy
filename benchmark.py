0e6c38585d8b4ced781c9f6c416c0002b8967b91
ATATC <futerry@outlook.com>
2025-12-20 16:40:55 -0500

    Added customizable benchmark functions (`full_volume`, `resize128`, `resize256`, `roi`) and introduced `UNetSlidingTrainer` for sliding window training. (#133)

--- benchmark.py
+++ benchmark.py
@@ -5,31 +5,88 @@ from os.path import exists
 from monai.transforms import Resize
 from torch.utils.data import DataLoader
 
-from mipcandy import Device, auto_device, download_dataset, NNUNetDataset
-from unet import UNetTrainer
+from mipcandy import Device, auto_device, download_dataset, NNUNetDataset, inspect, InspectionAnnotations, \
+    load_inspection_annotations, ROIDataset
+from unet import UNetTrainer, UNetSlidingTrainer
+
+BENCHMARK_DATASET: str = "nnunet_datasets/AbdomenCT-1K-ss1"
+BENCHMARK_NUM_CLASSES: int = 5
 
 
-def main(input_folder: str | PathLike[str], output_folder: str | PathLike[str], *, num_epochs: int = 100,
+def inspect_dataset(dataset: NNUNetDataset, output_folder: str | PathLike[str]) -> InspectionAnnotations:
+    if exists(f"{output_folder}/annotations.json"):
+        return load_inspection_annotations(f"{output_folder}/annotations.json", dataset)
+    annotations = inspect(dataset)
+    annotations.save(f"{output_folder}/annotations.json")
+    return annotations
+
+
+def full_volume(input_folder: str | PathLike[str], output_folder: str | PathLike[str], *, num_epochs: int = 100,
          device: Device | None = None) -> None:
     if not device:
         device = auto_device()
     if not exists(f"{input_folder}/dataset"):
-        download_dataset("nnunet_datasets/AbdomenCT-1K-ss1", f"{input_folder}/dataset")
-    trans = Resize((128, 128, 128))
-    dataset = NNUNetDataset(f"{input_folder}/dataset", image_transform=trans, label_transform=trans)
+        download_dataset(f"nnunet_datasets/{BENCHMARK_DATASET}", f"{input_folder}/dataset")
+    dataset = NNUNetDataset(f"{input_folder}/dataset")
     train, val = dataset.fold()
     train_loader = DataLoader(train, batch_size=1, shuffle=True)
     val_loader = DataLoader(val, batch_size=1, shuffle=False)
+    trainer = UNetSlidingTrainer(output_folder, train_loader, val_loader, recoverable=False, device=device)
+    trainer.num_classes = BENCHMARK_NUM_CLASSES
+    trainer.train(num_epochs, note="MIP Candy Benchmark - full size")
+
+
+def resize(size: int, input_folder: str | PathLike[str], output_folder: str | PathLike[str], *, num_epochs: int = 100,
+           device: Device | None = None) -> None:
+    if not device:
+        device = auto_device()
+    if not exists(f"{input_folder}/dataset"):
+        download_dataset(f"nnunet_datasets/{BENCHMARK_DATASET}", f"{input_folder}/dataset")
+    trans = Resize((size, size, size))
+    dataset = NNUNetDataset(f"{input_folder}/dataset", image_transform=trans, label_transform=trans)
+    train, val = dataset.fold()
+    train_loader = DataLoader(train, batch_size=2, shuffle=True)
+    val_loader = DataLoader(val, batch_size=1, shuffle=False)
     trainer = UNetTrainer(output_folder, train_loader, val_loader, recoverable=False, device=device)
-    trainer.train(num_epochs, note="MIP Candy Benchmark")
+    trainer.num_classes = BENCHMARK_NUM_CLASSES
+    trainer.train(num_epochs, note=f"MIP Candy Benchmark - resize{size}")
+
+
+def resize128(input_folder: str | PathLike[str], output_folder: str | PathLike[str], *, num_epochs: int = 100,
+              device: Device | None = None) -> None:
+    resize(128, input_folder, output_folder, num_epochs=num_epochs, device=device)
+
+
+def resize256(input_folder: str | PathLike[str], output_folder: str | PathLike[str], *, num_epochs: int = 100,
+              device: Device | None = None) -> None:
+    resize(256, input_folder, output_folder, num_epochs=num_epochs, device=device)
+
+
+def roi(input_folder: str | PathLike[str], output_folder: str | PathLike[str], *, num_epochs: int = 100,
+        device: Device | None = None) -> None:
+    if not device:
+        device = auto_device()
+    if not exists(f"{input_folder}/dataset"):
+        download_dataset(f"nnunet_datasets/{BENCHMARK_DATASET}", f"{input_folder}/dataset")
+    dataset = NNUNetDataset(f"{input_folder}/dataset")
+    annotations = inspect_dataset(dataset, output_folder)
+    dataset = ROIDataset(annotations)
+    train, val = dataset.fold()
+    train_loader = DataLoader(train, batch_size=2, shuffle=True)
+    val_loader = DataLoader(val, batch_size=1, shuffle=False)
+    trainer = UNetTrainer(output_folder, train_loader, val_loader, recoverable=False, device=device)
+    trainer.num_classes = BENCHMARK_NUM_CLASSES
+    trainer.train(num_epochs, note=f"MIP Candy Benchmark - roi")
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(prog="MIP Candy Benchmark", description="MIP Candy Benchmark",
                             epilog="GitHub: https://github.com/ProjectNeura/MIPCandy")
+    parser.add_argument("test", choices=("full", "resize128", "resize256", "roi"))
     parser.add_argument("-i", "--input-folder")
     parser.add_argument("-o", "--output-folder")
     parser.add_argument("--num-epochs", type=int, default=100)
     parser.add_argument("--device", default=None)
     args = parser.parse_args()
-    main(args.input_folder, args.output_folder, num_epochs=args.num_epochs, device=args.device)
+    test = locals()[args.test]
+    test(args.input_folder, args.output_folder, num_epochs=args.num_epochs, device=args.device)
--- unet.py
+++ unet.py
@@ -3,14 +3,10 @@ from typing import override
 from monai.networks.nets import BasicUNet
 from torch import nn
 
-from mipcandy import SegmentationTrainer, AmbiguousShape, DiceBCELossWithLogits
+from mipcandy import SegmentationTrainer, SlidingSegmentationTrainer, AmbiguousShape, DiceBCELossWithLogits
 
 
 class UNetTrainer(SegmentationTrainer):
-    sliding_window_shape = (64, 64, 64)
-    sliding_window_batch_size = 1
-    num_classes = 5
-
     @override
     def build_criterion(self) -> nn.Module:
         return DiceBCELossWithLogits(self.num_classes, include_bg=False)
@@ -18,3 +14,8 @@ class UNetTrainer(SegmentationTrainer):
     @override
     def build_network(self, example_shape: AmbiguousShape) -> nn.Module:
         return BasicUNet(3, example_shape[0], 5)
+
+
+class UNetSlidingTrainer(SlidingSegmentationTrainer, UNetTrainer):
+    sliding_window_shape = (64, 64, 64)
+    sliding_window_batch_size = 1
