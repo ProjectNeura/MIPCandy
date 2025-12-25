aa5008f147fa02e52dfe7a1f96eb44eb32bb758c
Tianhao Fu <futerry@outlook.com>
2025-12-25 02:00:31 -0500

    Fix "Support joining transforms" (#184)
    * Revert "Extended transform type compatibility with MONAI and add `join_transf…"
    
    This reverts commit 607a8a52ee42fffa9c8518bed3a14077e203c9bb.
    
    * Reapply "Extended transform type compatibility with MONAI and add `join_transf…"
    
    This reverts commit ef93eb9e9ec0388de8ceb4aa8d1098b37799ff60.
    
    * Updated `JointTransform` to support customizable keys and introduce `MONAITransform`. (#182)

--- mipcandy/data/transform.py
+++ mipcandy/data/transform.py
@@ -6,22 +6,34 @@ from mipcandy.types import Transform
 
 class JointTransform(nn.Module):
     def __init__(self, *, transform: Transform | None = None, image_only: Transform | None = None,
-                 label_only: Transform | None = None) -> None:
+                 label_only: Transform | None = None, keys: tuple[str, str] = ("image", "label")) -> None:
         super().__init__()
         self._transform: Transform | None = transform
         self._image_only: Transform | None = image_only
         self._label_only: Transform | None = label_only
+        self._keys: tuple[str, str] = keys
 
     def __call__(self, image: torch.Tensor, label: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
-        data = {"image": image, "label": label}
+        ik, lk = self._keys
+        data = {ik: image, lk: label}
         if self._transform:
             data = self._transform(data)
         if self._image_only:
-            data["image"] = self._image_only(data["image"])
+            data[ik] = self._image_only(data[ik])
         if self._label_only:
-            data["label"] = self._label_only(data["label"])
-        return data["image"], data["label"]
+            data[lk] = self._label_only(data[lk])
+        return data[ik], data[lk]
 
 
-def join_transform(transform: Transform) -> nn.Module:
-    return JointTransform(image_only=transform, label_only=transform)
+class MONAITransform(nn.Module):
+    def __init__(self, transform: Transform, *, keys: tuple[str, str] = ("image", "label")) -> None:
+        super().__init__()
+        self._transform: Transform = transform
+        self._keys: tuple[str, str] = keys
+
+    def forward(self, data: torch.Tensor | dict[str, torch.Tensor]) -> torch.Tensor | dict[str, torch.Tensor]:
+        if isinstance(data, torch.Tensor):
+            return self._transform(data)
+        ik, lk = self._keys
+        image, label = data[ik], data[lk]
+        return {ik: self._transform(image), lk: self._transform(label)}
