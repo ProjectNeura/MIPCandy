607a8a52ee42fffa9c8518bed3a14077e203c9bb
Tianhao Fu <futerry@outlook.com>
2025-12-25 01:51:07 -0500

    Extended transform type compatibility with MONAI and add `join_transform()` utility. (#182) (#183)

--- mipcandy/data/transform.py
+++ mipcandy/data/transform.py
@@ -5,18 +5,23 @@ from mipcandy.types import Transform
 
 
 class JointTransform(nn.Module):
-    def __init__(self, transform: Transform, *, image_only: Transform | None = None,
+    def __init__(self, *, transform: Transform | None = None, image_only: Transform | None = None,
                  label_only: Transform | None = None) -> None:
         super().__init__()
-        self._transform: Transform = transform
+        self._transform: Transform | None = transform
         self._image_only: Transform | None = image_only
         self._label_only: Transform | None = label_only
 
     def __call__(self, image: torch.Tensor, label: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
         data = {"image": image, "label": label}
-        data = self._transform(data)
+        if self._transform:
+            data = self._transform(data)
         if self._image_only:
             data["image"] = self._image_only(data["image"])
         if self._label_only:
             data["label"] = self._label_only(data["label"])
         return data["image"], data["label"]
+
+
+def join_transform(transform: Transform) -> nn.Module:
+    return JointTransform(image_only=transform, label_only=transform)
