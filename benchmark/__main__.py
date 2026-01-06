9349ae84e0d929753cdec617dbc4c2d75637562a
ATATC <futerry@outlook.com>
2026-01-06 12:45:08 -0500

    Expanded benchmarking utility to support `Training` and `SlidingTraining` tests, updating CLI argument handling accordingly. (#133)

--- benchmark/__main__.py
+++ benchmark/__main__.py
@@ -2,14 +2,20 @@ from argparse import ArgumentParser
 from os.path import exists
 
 from benchmark.data import DataTest, SlidingWindowTest
+from benchmark.training import TrainingTest, SlidingTrainingTest
 from mipcandy import auto_device, download_dataset, Frontend, NotionFrontend, WandBFrontend
 
 BENCHMARK_DATASET: str = "AbdomenCT-1K-ss1"
 
 if __name__ == "__main__":
+    tests = test = {
+        "SlidingWindow": SlidingWindowTest,
+        "Training": TrainingTest,
+        "SlidingTraining": SlidingTrainingTest
+    }
     parser = ArgumentParser(prog="MIP Candy Benchmark", description="MIP Candy Benchmark",
                             epilog="GitHub: https://github.com/ProjectNeura/MIPCandy")
-    parser.add_argument("test", choices=("SlidingWindow",))
+    parser.add_argument("test", choices=tests.keys())
     parser.add_argument("-i", "--input-folder")
     parser.add_argument("-o", "--output-folder")
     parser.add_argument("--num-epochs", type=int, default=100)
@@ -17,9 +23,7 @@ if __name__ == "__main__":
     parser.add_argument("--front-end", choices=(None, "n", "w"), default=None)
     args = parser.parse_args()
     DataTest.dataset = BENCHMARK_DATASET
-    test = {
-        "SlidingWindow": SlidingWindowTest
-    }[args.test](
+    tests[args.test](
         args.input_folder, args.output_folder, args.num_epochs, args.device if args.device else auto_device(), {
             None: Frontend, "n": NotionFrontend, "w": WandBFrontend
         }[args.front_end]
