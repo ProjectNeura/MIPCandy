0083f3d861b88b2ca05a4480d205973d1ded2919
ATATC <futerry@outlook.com>
2026-01-08 14:01:14 -0500

    Relocated `empty_cache` call within validation loop to optimize memory usage and removed redundant call outside loop.

--- mipcandy/training.py
+++ mipcandy/training.py
@@ -538,6 +538,7 @@ class Trainer(WithPaddingModule, WithNetwork, metaclass=ABCMeta):
                 image, label = image.squeeze(0), label.squeeze(0)
                 progress.update(task, description=f"Validating case {idx} {tuple(image.shape)}")
                 case_score, case_metrics, output = self.validate_case(idx, image, label, toolbox)
+                self.empty_cache()
                 score += case_score
                 if case_score < worst_score:
                     self._tracker.worst_case = idx
@@ -546,7 +547,6 @@ class Trainer(WithPaddingModule, WithNetwork, metaclass=ABCMeta):
                 try_append_all(case_metrics, metrics)
                 progress.update(task, advance=1, description=f"Validating case {idx} ({case_score:.4f})")
                 idx += 1
-                self.empty_cache()
         self.empty_cache()
         return score / num_cases, metrics
 
