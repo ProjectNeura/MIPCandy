14b5f095e5558d0ed889ada32ead2c9d140e2d52
ATATC <futerry@outlook.com>
2026-01-26 21:12:10 -0500

    Added `DiceCELossWithLogits` class to `loss.py` for combined Cross Entropy and Soft Dice loss; refined `convert_ids_to_logits` to use `torch.no_grad` for improved performance.

--- mipcandy/common/optim/loss.py
+++ mipcandy/common/optim/loss.py
@@ -24,6 +24,33 @@ class FocalBCEWithLogits(nn.Module):
         return do_reduction(loss, self.reduction)
 
 
+class DiceCELossWithLogits(nn.Module):
+    def __init__(self, num_classes: int, *, lambda_ce: float = 1, lambda_soft_dice: float = 1,
+                 smooth: float = 1, include_background: bool = True) -> None:
+        super().__init__()
+        self.num_classes: int = num_classes
+        self.lambda_ce: float = lambda_ce
+        self.lambda_soft_dice: float = lambda_soft_dice
+        self.smooth: float = smooth
+        self.include_background: bool = include_background
+
+    def forward(self, masks: torch.Tensor, labels: torch.Tensor) -> tuple[torch.Tensor, dict[str, float]]:
+        if self.num_classes != 1 and labels.shape[1] == 1:
+            d = labels.ndim - 2
+            if d not in (1, 2, 3):
+                raise ValueError(f"Expected labels to be 1D, 2D, or 3D, got {d} spatial dimensions")
+            with torch.no_grad():
+                logit_labels = convert_ids_to_logits(labels.int(), d, self.num_classes)
+        else:
+            logit_labels = labels.float()
+        ce = nn.functional.cross_entropy(masks, labels[:, 0])
+        masks = masks.sigmoid()
+        soft_dice = soft_dice_coefficient(masks, logit_labels, smooth=self.smooth,
+                                          include_background=self.include_background)
+        c = self.lambda_ce * ce - self.lambda_soft_dice * soft_dice
+        return c, {"soft dice": soft_dice.item(), "ce loss": ce.item()}
+
+
 class DiceBCELossWithLogits(nn.Module):
     def __init__(self, num_classes: int, *, lambda_bce: float = 1, lambda_soft_dice: float = 1,
                  smooth: float = 1, include_background: bool = True) -> None:
@@ -39,7 +66,8 @@ class DiceBCELossWithLogits(nn.Module):
             d = labels.ndim - 2
             if d not in (1, 2, 3):
                 raise ValueError(f"Expected labels to be 1D, 2D, or 3D, got {d} spatial dimensions")
-            labels = convert_ids_to_logits(labels.int(), d, self.num_classes)
+            with torch.no_grad():
+                labels = convert_ids_to_logits(labels.int(), d, self.num_classes)
         labels = labels.float()
         bce = nn.functional.binary_cross_entropy_with_logits(masks, labels)
         masks = masks.sigmoid()
