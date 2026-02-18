a189344f9a9a7fd32686f99236064d6a7bd381be
ATATC <futerry@outlook.com>
2026-02-18 01:57:15 -0500

    Refactored `_SegmentationLoss` to improve structure; incorporated `dice_similarity_coefficient_with_logits`. (#205)

--- mipcandy/common/optim/loss.py
+++ mipcandy/common/optim/loss.py
@@ -4,7 +4,8 @@ import torch
 from torch import nn
 
 from mipcandy.data import convert_ids_to_logits, convert_logits_to_ids
-from mipcandy.metrics import do_reduction, soft_dice_coefficient, dice_similarity_coefficient_binary
+from mipcandy.metrics import do_reduction, soft_dice_coefficient, dice_similarity_coefficient_binary, \
+    dice_similarity_coefficient_with_logits
 
 
 class FocalBCEWithLogits(nn.Module):
@@ -25,39 +26,41 @@ class FocalBCEWithLogits(nn.Module):
 
 
 class _Loss(nn.Module):
-    def __init__(self, include_background: bool) -> None:
+    def __init__(self) -> None:
         super().__init__()
         self.validation_mode: bool = False
+
+
+class _SegmentationLoss(_Loss):
+    def __init__(self, num_classes: int, include_background: bool) -> None:
+        super().__init__()
+        self.num_classes: int = num_classes
         self.include_background: bool = include_background
+
+    def logitfy(self, ids: torch.Tensor) -> torch.Tensor:
+        with torch.no_grad():
+            if self.num_classes != 1 and ids.shape[1] == 1:
+                if (d := ids.ndim - 2) not in (1, 2, 3):
+                    raise ValueError(f"Expected labels to be 1D, 2D, or 3D, got {d} spatial dimensions")
+                return convert_ids_to_logits(ids.int(), d, self.num_classes)
+        return ids.float()
 
     def forward(self, outputs: torch.Tensor, labels: torch.Tensor) -> tuple[torch.Tensor, dict[str, float]]:
         if not self.validation_mode:
             return self._forward(outputs, labels)
         with torch.no_grad():
             c, metrics = self._forward(outputs, labels)
+            if (d := labels.ndim - 2) not in (1, 2, 3):
+                raise ValueError(f"Expected labels to be 1D, 2D, or 3D, got {d} spatial dimensions")
             outputs = convert_logits_to_ids(outputs)
             dice = 0
             for i in range(0 if self.include_background else 1, self.num_classes):
                 class_dice = dice_similarity_coefficient_binary(outputs == i, labels == i).item()
                 dice += class_dice
                 metrics[f"dice {i}"] = class_dice
-            metrics["dice"] = dice / (self.num_classes - (0 if self.include_background else 1))
+            outputs = self.logitfy(outputs)
+            metrics["dice"] = dice_similarity_coefficient_with_logits(outputs, labels).item()
             return c, metrics
-
-
-class _SegmentationLoss(_Loss):
-    def __init__(self, num_classes: int, include_background: bool) -> None:
-        super().__init__(include_background)
-        self.num_classes: int = num_classes
-
-    def logitfy(self, labels: torch.Tensor) -> torch.Tensor:
-        with torch.no_grad():
-            if self.num_classes != 1 and labels.shape[1] == 1:
-                d = labels.ndim - 2
-                if d not in (1, 2, 3):
-                    raise ValueError(f"Expected labels to be 1D, 2D, or 3D, got {d} spatial dimensions")
-                return convert_ids_to_logits(labels.int(), d, self.num_classes)
-        return labels.float()
 
 
 class DiceCELossWithLogits(_SegmentationLoss):
